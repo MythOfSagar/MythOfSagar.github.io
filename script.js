c68ca988d930cc69580b66ba43c88a95b7eee5aa
sagar5233213 <sagar@zendynamix.com>
2025-01-12 20:54:19 +0530

    updated details

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 const sideMenu = document.getElementById("sideMenu");
 const USERNAME = 'MythOfSagar'
 
-const openMenu = () => 
-{
+const openMenu = () => {
   sideMenu.style.right = "0";
 }
 
@@ -17,54 +16,45 @@ const clientMessage = document.querySelector("#clientMessage");
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (e) => 
-{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const name = clientName.value;
   const email = clientEmail.value;
   const message = clientMessage.value;
 
-  emailjs.send(serviceID, templateID, 
-  {
-    name,
-    email,
-    message,
-  }).then((response) => 
+  emailjs.send(serviceID, templateID,
     {
-    if (response.text === 'OK') 
-    {
-      alert("Message sent successfully")
-    } else 
-    {
+      name,
+      email,
+      message,
+    }).then((response) => {
+      if (response.text === 'OK') {
+        alert("Message sent successfully")
+      } else {
+        alert("Error Occured Please Contact me through email")
+      };
+    }).catch((error) => {
       alert("Error Occured Please Contact me through email")
-    };
-  }).catch((error) => 
-  {
-    alert("Error Occured Please Contact me through email")
-  });
+    });
 });
 
-const closeMenu = () => 
-{
+const closeMenu = () => {
   sideMenu.style.right = "-120px";
 }
 
 
-const openResume = () => 
-{
+const openResume = () => {
   window.open(
     "https://drive.google.com/file/d/1fNp6Hs_BnTyJ8irjtbHlPhkTkV_qJxzC/view?usp=share_link"
   );
 }
 
-const addSkills = (dataSkills) => 
-{
+const addSkills = (dataSkills) => {
 
   const skillsContainer = document.getElementById("skills_learnedDiv");
 
-  dataSkills.map((skill) => 
-  {
+  dataSkills.map((skill) => {
     const div = document.createElement("div");
     const image = document.createElement("img");
     const title = document.createElement("h3");
@@ -77,82 +67,75 @@ const addSkills = (dataSkills) =>
   });
 }
 
-const handlePortfolioTitle = () => 
-{
+const handlePortfolioTitle = () => {
   const portfolioTitle = document.querySelector('#portfolio-title')
   const defaultTitle = 'Sagar'
-  if (typeof document.hidden !== "undefined") 
-  {
+  if (typeof document.hidden !== "undefined") {
 
-    const handleVisibilityChange = () => 
-    {
-      if (document['hidden']) 
-      {
+    const handleVisibilityChange = () => {
+      if (document['hidden']) {
         portfolioTitle.innerText = 'The One U R Looking For'
-      } else 
-      {
+      } else {
         portfolioTitle.innerText = defaultTitle
       }
     }
 
     document.addEventListener('visibilitychange', handleVisibilityChange, false);
-  } else 
-  {
+  } else {
     // Page Visibility API is not supported
     portfolioTitle.innerText = defaultTitle
   }
 }
 
-const assignLinks = (username) =>{
+const assignLinks = (username) => {
 
   const githubProfile = document.querySelectorAll('.githubProfile');
-  githubProfile.forEach(p=>{
-    p.setAttribute('href',`https://github.com/${username}`);
+  githubProfile.forEach(p => {
+    p.setAttribute('href', `https://github.com/${username}`);
   })
 
   const linkedinProfile = document.querySelectorAll('.linkedinProfile');
-  linkedinProfile.forEach(p=>{
-    p.setAttribute('href',`https://linkedin.com/in/${username}`);
+  linkedinProfile.forEach(p => {
+    p.setAttribute('href', `https://linkedin.com/in/${username}`);
   })
 
   const telegramProfile = document.querySelectorAll('.telegramProfile');
-  telegramProfile.forEach(p=>{
-    p.setAttribute('href',`https://t.me/${username}`);
+  telegramProfile.forEach(p => {
+    p.setAttribute('href', `https://t.me/${username}`);
   })
 
   const leetcodeProfile = document.querySelectorAll('.leetcodeProfile');
-  leetcodeProfile.forEach(p=>{
-    p.setAttribute('href',`https://leetcode.com/${username}`);
+  leetcodeProfile.forEach(p => {
+    p.setAttribute('href', `https://leetcode.com/${username}`);
   })
-  
+
   const githubCard1 = document.querySelector('#githubCard1');
   const githubCard2 = document.querySelector('#githubCard2');
 
-  githubCard1.setAttribute('src',`http://github-profile-summary-cards.vercel.app/api/cards/stats?username=${username}&theme=solarized_dark`);
-  githubCard2.setAttribute('src',`http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${username}&theme=solarized_dark`);
+  githubCard1.setAttribute('src', `http://github-profile-summary-cards.vercel.app/api/cards/stats?username=${username}&theme=solarized_dark`);
+  githubCard2.setAttribute('src', `http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${username}&theme=solarized_dark`);
 
   setTimeout(() => {
-  
+
     const img = document.createElement('img');
-  
+
     img.src = `https://leetcard.jacoblin.cool/${username}?theme=nord`
     img.title = "Click to view LeetCode Profile"
 
     const leetcodeCard = document.querySelector('.leetcodeCard');
     leetcodeCard.append(img)
-  },1)
+  }, 1)
 }
 
 
 
-const addProjects = (projects) => 
-{
+const addProjects = (projects) => {
   const projects_made__worklist = document.querySelector('.projects_made__worklist')
-  projects.forEach(({image,title,deployedLink,desc,techStack,repoLink}) => {
-    const individualProject =  document.createElement('div');
+  projects.forEach(({ image, title, deployedLink, desc, techStack, repoLink }) => {
+    const individualProject = document.createElement('div');
     individualProject.className = 'individualProject';
     individualProject.innerHTML =
-    `<div class="projects_made__worklist-work">
+      `<div class="projects_made__worklist-work">
       <img src="${image}" alt="${title}" />
       <div class="projects_made__worklist-layer">
         <h2>${title}</h2>
@@ -167,24 +150,14 @@ const addProjects = (projects) =>
     <h4>${desc}</h4>
     <h4><span>Tech Stack:</span>${techStack}</h4>`
 
-  projects_made__worklist.append(individualProject)
+    projects_made__worklist.append(individualProject)
   })
 
- 
+
 }
 
 
 const projects = [
-  {
-    image: 'img/fashionHunter.png',
-    title: 'Fashion Hunter',
-    repoLink: `https://github.com/${USERNAME}/fashion_hunter_project`,
-    deployedLink: 'https://fashion-hunter.netlify.app',
-    techStack: 'React | Redux | Chakra UI | ExpressJS | MongoDB',
-    desc: `Fashion Hunter is an e-commerce website,
-  on which I have developed
-  admin page and a payment page.`
-  },
   {
     image: 'https://i.ibb.co/TYqLBMf/ICEMELON.png',
     title: 'Ice Melon',
@@ -195,24 +168,51 @@ const projects = [
     refreshing mix of diverse topics.`
   },
   {
-    image: 'img/indeed.png',
-    title: 'Indeed Clone',
-    repoLink: `https://github.com/${USERNAME}/indeed_project`,
-    deployedLink: 'https://strong-rolypoly-7a7c64.netlify.app',
-    techStack: 'HTML | CSS | JavaScript',
-    desc: `Indeed.com is Online Job and Recruitment Portal,
-    on which I have developed signup/login functionality.`
+    image: 'img/unlimited-cats.png',
+    title: 'Unlimited Cats',
+    repoLink: `https://github.com/${USERNAME}/infinite-scrolling`,
+    deployedLink: 'https://unlimitedcats.netlify.app',
+    techStack: 'React | TypeScript',
+    desc: `Unlimited Cats: Where scrolling feels as endless as a cat's curiosity ! Dive into a purr-fectly delightful abyss of feline frolics and whisker wonders.`
   },
   {
-    image: 'img/awakeFashion.png',
-    title: 'Awake Fashion',
-    repoLink: `https://github.com/${USERNAME}/awake_fashion_project`,
-    deployedLink: 'https://awake-fashion.netlify.app',
-    techStack: 'React | Redux | Chakra UI',
-    desc: `Awake Fashion is an e-commerce website,
-    on which I have developed
-    admin page and a payment page.`
-  }
+    image: 'img/asos.png',
+    title: 'Asos',
+    repoLink: `https://github.com/${USERNAME}/asos_project`,
+    deployedLink: 'https://stirring-marzipan-5d4585.netlify.app',
+    techStack: 'HTML | CSS | JavaScript',
+    desc: `Asos is an e-commerce website. This website is an online shopping platform. 
+    We cloned this website the same as the original website..`
+  },
+  {
+    image: 'img/fashionHunter.png',
+    title: 'Fashion Hunter',
+    repoLink: `https://github.com/${USERNAME}/fashion_hunter_project`,
+    deployedLink: 'https://fashion-hunter.netlify.app',
+    techStack: 'React | Redux | Chakra UI | ExpressJS | MongoDB',
+    desc: `Fashion Hunter is an e-commerce website,
+  on which I have developed
+  admin page and a payment page.`
+  },
+  // {
+  //   image: 'img/indeed.png',
+  //   title: 'Indeed Clone',
+  //   repoLink: `https://github.com/${USERNAME}/indeed_project`,
+  //   deployedLink: 'https://strong-rolypoly-7a7c64.netlify.app',
+  //   techStack: 'HTML | CSS | JavaScript',
+  //   desc: `Indeed.com is Online Job and Recruitment Portal,
+  //   on which I have developed signup/login functionality.`
+  // },
+  // {
+  //   image: 'img/awakeFashion.png',
+  //   title: 'Awake Fashion',
+  //   repoLink: `https://github.com/${USERNAME}/awake_fashion_project`,
+  //   deployedLink: 'https://awake-fashion.netlify.app',
+  //   techStack: 'React | Redux | Chakra UI',
+  //   desc: `Awake Fashion is an e-commerce website,
+  //   on which I have developed
+  //   admin page and a payment page.`
+  // }
 ];
 
 const skills = [
